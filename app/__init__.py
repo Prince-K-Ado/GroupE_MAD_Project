d86f207200bfff81d6f81f124e950351ae504f9e
danduypham <danduypham@gmail.com>
2025-04-01 20:57:10 -0700

    Add files via upload
    Adding successful post option and page

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
+from flask_migrate import Migrate
 
 #load_dotenv()
 # Create one instance of SQLAlchemy
@@ -12,7 +13,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 # Bind SQLAlchemy to this app
 db = SQLAlchemy(app)
-
+migrate = Migrate(app, db)
 # db.init_app(app)
 # Import models to register them with SQLAlchemy
 from app import models
@@ -20,5 +21,4 @@ from app import models
 # Register your blueprints
 from app.routes import main as main_blueprint
 app.register_blueprint(main_blueprint)
-
 
